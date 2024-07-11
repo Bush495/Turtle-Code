193be2c745f06721ded3afa26a15c03fba0804b5
Bush495 <bryleydsmith@gmail.com>
2024-07-10 22:47:32 -0700

    Add files via upload

--- Online.lua
+++ Online.lua
@@ -26,6 +26,14 @@ function Inspect(Tunnel, veinMineCheck)
     if Tunnel == true then
         lineMine(forwardData, leftData, backData, rightData, upData, downData, Valuables)
     elseif veinMineCheck == true then    
+        Path = {
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        }
         Path = veinMine(forwardData, leftData, backData, rightData, upData, downData, Valuables)
         for i, value in pairs(Valuables) do   
             if (forwardData["name"] ~= ("minecraft:" .. Valuables[i])) and (leftData["name"] ~= ("minecraft:" .. Valuables[i])) and (backData["name"] ~= (("minecraft:" .. Valuables[i])) and rightData["name"] ~= ("minecraft:" .. Valuables[i])) and (upData["name"] ~= ("minecraft:" .. Valuables[i])) and (backData["name"] ~= ("minecraft:" .. Valuables[i])) then
@@ -86,6 +94,14 @@ function veinMineMain()
     veinMineCheck = true
     Run = 0
     repeat
+        newPath = {
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+        }
         Path = {
             0,
             0,
